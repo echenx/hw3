8305e8b44d5668561851889d2db3b12bbf2e1a48
evan <evanchenx7@gmail.com>
2024-10-13 20:08:58 -0400

    implemented state management

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => GameState(),
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -15,50 +21,45 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Card Flip Game'),
+      home: const MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
 
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
- 
   @override
   Widget build(BuildContext context) {
+    final gameState = Provider.of<GameState>(context);
+
     return Scaffold(
       appBar: AppBar(title: const Text('Card Flip Game')),
       body: GridView.builder(
         padding: const EdgeInsets.all(5),
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 4, 
+          crossAxisCount: 4,
           crossAxisSpacing: 5,
           mainAxisSpacing: 10,
         ),
-        itemCount: 16, 
+        itemCount: 16,
         itemBuilder: (context, index) {
-          return Container(
-            width: 100,
-            height: 100,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(50),
-              image: const DecorationImage(
-                image: NetworkImage('https://media.istockphoto.com/id/459234653/photo/back-side-of-playing-card.jpg?s=612x612&w=0&k=20&c=U1YUsBOZuuKgeXW6yT3dUZmQFBuzEooRUQJrmvGT1DA='), 
+          final card = gameState.cards[index];
+          return GestureDetector(
+            onTap: () {
+              gameState.flipCard(index);
+            },
+            child: Container(
+              width: 100,
+              height: 100,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Colors.transparent, // Set the color to transparent
+                image: DecorationImage(
+                  image: NetworkImage(
+                    card.isFaceUp ? card.frontImage : '/assets/cardback.jpg',
+                  ),
+                ),
               ),
             ),
           );
@@ -66,4 +67,42 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
     );
   }
+}
+
+class GameState extends ChangeNotifier {
+  List<Card> cards = [];
+
+  GameState() {
+    _initializeCards();
+  }
+
+  void _initializeCards() {
+    List<String> images = [
+      'https://picsum.photos/id/1/200/300',
+      'https://picsum.photos/id/2/200/300',
+      'https://picsum.photos/id/3/200/300',
+      'https://picsum.photos/id/4/200/300',
+      'https://picsum.photos/id/5/200/300',
+      'https://picsum.photos/id/6/200/300',
+      'https://picsum.photos/id/7/200/300',
+      'https://picsum.photos/id/8/200/300',
+    ];
+
+    List<String> cardImages = List.from(images)..addAll(images);
+    cardImages.shuffle();
+
+    cards = cardImages.take(16).map((image) => Card(image)).toList();
+  }
+
+  void flipCard(int index) {
+    cards[index].isFaceUp = !cards[index].isFaceUp;
+    notifyListeners();
+  }
+}
+
+class Card {
+  final String frontImage;
+  bool isFaceUp = false;
+
+  Card(this.frontImage);
 }
